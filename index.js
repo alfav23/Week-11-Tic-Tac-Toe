9aab49259f1e1a081c2773aefeea00f58f4f0e99
Alyssa Favorito <alyssafavorito2000@gmail.com>
2023-12-22 14:07:20 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+// make main header change between players based on turn
+// make an on-click event to generate x or o based on turn
+// make a reset button
+// alert for a draw and alert for winner
